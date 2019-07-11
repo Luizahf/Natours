e365a036bfa27d0e575ac77e7a2f310be6ebe383
Luizahf <luizahfreire@hotmail.com>
2019-07-11 13:17:06 -0300

    Implemented validations for ID, name and price in controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,27 @@
 const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if(req.params.id*1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+};
+
+exports.checkBody = (req, res, next) => {
+    if(!req.body.name || !req.body.price)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name ou price'
+        });
+    }
+    next();
+};
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -17,13 +38,6 @@ exports.getTour = (req, res) => {
     console.log(req.params);
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id == id);
-    // if(id > tours.length) {
-    if(!tour) {
-    return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID'
-    });
-    }
 
     res.status(200).json({
         status: 'success',
@@ -49,13 +63,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updatetour = (req, res) => {
-    if(req.params.id*1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(200).json({
         status: 'success',
         data: {
@@ -65,12 +72,6 @@ exports.updatetour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-    if(req.params.id*1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
 
     res.status(204).json({
         status: 'success',
